36263473d266b163d24e7023a16d31cbed93ebc3
AnthonyK213 <tjjzqwh@163.com>
2023-03-15 23:22:06 +0800

    v0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::ffi::{c_char, CStr, CString};
 
 #[macro_export]
-macro_rules! string_parse {
+macro_rules! str_try_parse {
     ($c_buf: expr, $default: expr) => {
         match c_buf_to_string($c_buf) {
             Ok(v) => v,
@@ -10,12 +10,12 @@ macro_rules! string_parse {
     };
 }
 
-pub fn c_buf_to_string(c_buf: *const c_char) -> Result<String, std::str::Utf8Error> {
+pub fn str_buf_to_string(c_buf: *const c_char) -> Result<String, std::str::Utf8Error> {
     Ok(unsafe { CStr::from_ptr(c_buf) }.to_str()?.to_owned())
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn string_free(s: *mut c_char) {
+pub unsafe extern "C" fn str_free(s: *mut c_char) {
     if s.is_null() {
         return;
     }
